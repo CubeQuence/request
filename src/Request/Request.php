beab0a73f8e4d67d5617734b59a553fdcd8a38be
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-04-07 11:10:25 +0200

    create new exceptions

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace CQ\Request;
 
+use CQ\Request\Exceptions\BadResponseException;
+use CQ\Request\Exceptions\ConnectException;
 use GuzzleHttp\Client;
+
+// TODO: wrap in my own exception
+use GuzzleHttp\Exception\BadResponseException as GuzzleBadResponseException;
+use GuzzleHttp\Exception\ConnectException as GuzzleConnectException;
 
 final class Request
 {
@@ -18,12 +24,28 @@ final class Request
         $client = new Client();
         $parsedPath = self::parsePath($path);
 
-        $response = $client->request($method, $parsedPath->path, [
-            'headers' => $headers,
-            'query' => $parsedPath->query,
-            'json' => $json,
-            'form_params' => $form,
-        ]);
+        try {
+            $response = $client->request($method, $parsedPath->path, [
+                'headers' => $headers,
+                'query' => $parsedPath->query,
+                'json' => $json,
+                'form_params' => $form,
+            ]);
+        } catch (GuzzleBadResponseException $error) {
+            $response = $error->getResponse();
+
+            throw new BadResponseException(
+                message: $response->getBody()->getContents(),
+                code: $response->getStatusCode(),
+                previous: $error
+            );
+        } catch (GuzzleConnectException $error) {
+            throw new ConnectException(
+                message: $error->getMessage(),
+                code: $error->getCode(),
+                previous: $error
+            );
+        }
 
         $output = $response->getBody()->getContents();
 
